163218886962abe39eafd24ff5ba5a81774853fc
frametee29 <teeraphat123@hotmail.com>
2021-04-20 12:22:06 +0700

    Home

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+@NgModule({
+  declarations: [],
+  imports: [NzButtonModule],
+  providers: [],
+  bootstrap: [],
+})
+export class HomeModule {}
---
+++ themeTest.preset.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+        extend: {
+            backgroundImage: theme => ({
+                'mountain': "url('/src/assets/images/backgroud_home.jpg')",
+            })
+        },
+    },
+    plugins: [],
+}
